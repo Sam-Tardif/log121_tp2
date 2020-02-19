619b4efec22f1bc5161dce96e495545525877a66
Ronin825 <oblivion_825@hotmail.com>
2020-02-18 22:24:25 -0500

    Eclipse Commit - 22:24

--- log121_tp2/src/log121_tp2/main.java
+++ log121_tp2/src/log121_tp2/main.java
@@ -1,10 +1,10 @@
-
+package log121_tp2;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello wordl!");
+		System.out.println("hello world!!");
 	}
 
 }
